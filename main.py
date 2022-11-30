b449cf0d46a078092c1ea62af7256df4808727ad
2pokek <alkuzmich6479@gmail.com>
2022-11-30 23:07:18 +0300

    1

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'
+}
